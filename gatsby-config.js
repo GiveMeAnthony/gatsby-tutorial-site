e9050de648cb06fe73bf9478d3b20a509b15da71
base2taiji <base2taiji@proton.me>
2024-07-06 20:57:31 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Project Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
