6b76f98dab8fcbe6d508994dc0b5e3f0a8fcf094
GiveMeAnthony <givemeanthonny@icloud.com>
2024-07-08 03:10:10 -0700

    Stylin (Finished Gatsby Tutorial Part 2)

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 export default function AboutPage() {
   return (
-    <main>
-        <h1>About Me</h1>
-        <Link to="/">Back to Home</Link>
-        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
   )
 }
 
